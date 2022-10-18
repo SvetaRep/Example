e3b71e8d693515d68f575e237460bf4f1acdd6d3
Svetlana <svetarepalenko@yandex.ru>
2022-10-19 00:31:19 +0300

    done

---
+++ 1/Program.cs
@@ -0,0 +1,15 @@
+﻿// // Задача 64: Задайте значения M и N. Напишите программу, которая выведет все натуральные числа в промежутке от M до N.
+// // M = 1; N = 5. -> ""1, 2, 3, 4, 5""
+// // M = 4; N = 8. -> ""4, 6, 7, 8""
+
+int m=0;
+int n=0;
+int.TryParse(Console.ReadLine(),out m);
+int.TryParse(Console.ReadLine(),out n);
+string PrintNumbers(int start,int end)
+{
+    if(start==end) 
+      return start.ToString();
+    return (start+" "+ PrintNumbers(start+1,end));
+}
+Console.Write(PrintNumbers(m,n));
